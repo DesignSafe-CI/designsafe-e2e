482e58c5cfe1dd7c90dd08b9b51197839be23e79
Frank Netscher <fnetscher@tacc.utexas.edu>
2018-10-02 15:55:32 -0500

    Improved in-line documentation on test.  Added a config.js file that will allow users to set environment variables.  Specifically, this now allows users to set a test user's username and password.

---
+++ config.js
@@ -0,0 +1,4 @@
+export default {
+  username: "",    // Set with your test user's username
+  password: ""     // Set with your test user's password
+};
--- testForMyDataListing.ts
+++ testForMyDataListing.ts
@@ -1,20 +1,26 @@
 import { Selector } from 'testcafe';
+import config from './config'; // Loads variables from config.js
 
+// Loads DesignSafe staging page
 fixture `My Data Listing`
   .page`https://designsafeci-dev.tacc.utexas.edu/`;
 
+// Sets elements that will be used during test execution
 const loginButton = Selector('.btn-group').find('a').withText('Log in');
 const dropdown = Selector('.dropdown').withText('Research Workbench');
 const dropdownMenuDataDepot = dropdown.find('li').withText('Data Depot');
 const myDataListing = Selector('tr');
 
+// test begins
 test('My Data Listing', async t => {
+  // Clicks login button on home page
   await t
-    .click(loginButton);
+    .click(loginButton);  
   
+  // Enters username and password set in config.js and clicks login button
   await t
-    .typeText('#username', 'XYZ') // Replace XYZ with your test user's username
-    .typeText('#password', 'XYZ') // Replace XYZ with your test user's password
+    .typeText('#username', `${config.username}`)
+    .typeText('#password', `${config.password}`) 
     .click('#login-btn');
   
   // If the user account needs approval, uncomment the line of code below.  
@@ -22,10 +28,13 @@ test('My Data Listing', async t => {
   /* await t
     .click('#approveAlways'); */
 
+  //Navigates through navbar ribbon and selects Data Depot
   await t
     .click(dropdown)
     .click(dropdownMenuDataDepot)
 
+  // Counts entries on My Data file listing and checks that there are
+  // less the count is less than or equal to 25 rows
   const tableCount = await myDataListing.count;
   await t.expect(tableCount).lte(25, 'More than 25 items in My Data listing');
 }); 
